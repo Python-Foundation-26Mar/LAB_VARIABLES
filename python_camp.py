43f37ed6e719c761091f6a7775e9815dda8ad78d
Jana <jannnna19@gmail.com>
2023-03-29 00:37:28 +0300

    solved lab2

---
+++ python_camp.py
@@ -0,0 +1,11 @@
+
+#Define a variable that has a value of your favourite football team.
+FavTeam = "Real Madrid"
+#Define a variable “x“ has an integer value and a variable “y” has a float value.
+x=10
+y=15.6
+#Assign One Value to Multiple Variables (choose random names & values).
+var1 , var2 , var3 = 22
+
+#Assign multiple Values to Multiple Variables (choose random names & values).
+G,name,age = "F","Jana",21
