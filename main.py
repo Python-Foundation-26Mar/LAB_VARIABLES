cf15f4360885c8af43f59524ffd5eb723043ac1a
Rahmah Alamri <rahmahmalamri@gmail.com>
2023-03-29 06:24:30 +0300

    solved the lab

---
+++ main.py
@@ -0,0 +1,13 @@
+my_favorite_team="Macnhester United"
+print (my_favorite_team)
+X:int=10
+Y:int=20
+print (X)
+print (Y)
+Rhmah=Sara=Reem=24
+print(Rhmah)
+print(Sara)
+print(Reem)
+Class1,Class2=60,40
+print(Class1)
+print(Class2)
