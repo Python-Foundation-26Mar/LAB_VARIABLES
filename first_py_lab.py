6817acf07598342a297b86b3e8b9b0f0f4c0987b
Raghad Alhuzali <alhuzaliraghad@gmail.com>
2023-03-29 05:26:54 +0300

    solve first lab in camp

---
+++ first_py_lab.py
@@ -0,0 +1,20 @@
+'''
+Define a variable that has a value of your favourite football team.
+Define a variable “x“ has an integer value and a variable “y” has a float value.
+Assign One Value to Multiple Variables (choose random names & values).
+Assign multiple Values to Multiple Variables (choose random names & values).
+'''
+
+# Define a variable that has a value of your favourite football team.
+f_team = 'ALHILAL'
+
+#Define a variable “x“ has an integer value and a variable “y” has a float value.
+x = 10
+
+y = 10.99
+
+#Assign One Value to Multiple Variables (choose random names & values).
+raghad = reem = rima = 'IS graduates'
+
+#Assign multiple Values to Multiple Variables (choose random names & values).
+first, second = 11, 22
