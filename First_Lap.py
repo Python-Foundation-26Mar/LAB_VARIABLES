f49329d227332d60b55b183cb7d216e072d8c47b
Rahaf <ro.56@outlook.sa>
2023-03-29 06:22:21 +0300

    solved the First lap

---
+++ First_Lap.py
@@ -0,0 +1,18 @@
+#This is firt Lap in Python ...
+
+my_name = "Rahaf Saleh"
+
+x = 25 
+y = 233.4
+
+#One Value to Multiple Variables
+Sara = Hind = Rana = 34 
+# multiple Values to Multiple Variables
+Ali, Nasser, Adel = 25, 32, 29
+
+
+'''
+print(my_name)
+print(Sara, Hind, Rana)
+
+'''
