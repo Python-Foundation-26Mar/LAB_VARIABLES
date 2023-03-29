a92510ef20154fa7015403945cb4758e619482d7
Rawan Alanazi <rawanalannzy4@gmail.com>
2023-03-29 07:13:42 +0300

    solved the lab

---
+++ main.py
@@ -0,0 +1,5 @@
+my_favorite_team = "nothing"    # Define a variable that has a value of your favourite football team.
+x = 55                          # Define a variable “x“  has an integer value and a variable “y” has a float value.
+y = 70
+var1 = var2 = var3 = 55         # Assign One Value to Multiple Variables (choose random names & values).
+city_Noura, city_Rawan = Riyahd, Arar # Assign multiple Values to Multiple Variables (choose random names & values).
