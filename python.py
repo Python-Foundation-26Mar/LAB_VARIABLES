075a6f0d58e9c1eaf4f3920172cc91710f073b48
Dana Almadani <Dma37592@marymount.edu>
2023-03-29 05:26:35 -0400

    solved the lab

---
+++ python.py
@@ -0,0 +1,19 @@
+# Define a variable that has a value of your favourite football team.
+fav_team = "Paris Saint Germain"
+
+# Define a variable "x" has an integer value and a variable "y" has a float value.
+x = 10
+y = 5
+
+# Assign One Value to Multiple Variables
+a = b = c = "hi"
+
+# Assign multiple Values to Multiple Variables
+name, age, city = "Dana", 22, "Jeddah"
+
+# Print the values of all the variables
+print("My favourite football team is", fav_team)
+print("The value of x is", x)
+print("The value of y is", y)
+print("The value of a, b, and c is", a)
+print("The value of name is", name, ", age is", age, ", and city is", city)
