636c9104ac64ab62c9d9ecb27b2db01a5608be62
Rahaf <ro.56@outlook.sa>
2023-03-30 12:53:21 +0300

    Solved Lap3

---
+++ Second_Lab.py
@@ -0,0 +1,28 @@
+if True:
+    print("this is a true condition")
+    print("another operation")
+
+print("out of the if body ")
+
+
+user_name = "Rahaf"
+password = 4380
+temperature = -30
+
+
+
+if user_name == "Rahaf":
+    print("Hi Rahaf")
+elif user_name == "Saleh":
+    print("Hi Saleh")
+else:
+    print("You are not allowed here")
+
+if temperature <= 30 and temperature > 10:
+    print("The weather is nice")
+elif temperature > 30:
+    print("The weather is Could")
+
+
+if not user_name == "Ali":
+    print("hello Anonymous")
